074d590751fb66e08f5bf605f4b1904e62ef1081
felix egli <antgeneral@gmail.com>
2013-02-17 14:00:54 +0100

    config added as revealing module pattern
    http://addyosmani.com/resources/essentialjsdesignpatterns/book/#revealingmodulepatternjavascript

--- Mini-SPA/Scripts/app/config.js
+++ Mini-SPA/Scripts/app/config.js
@@ -1,13 +1,23 @@
-﻿var startupUrl = '#/list';
+﻿
+var config = function() {
 
-var allRoutes =
-[
-	{
-		routePath: '#/list',
-		view: '#content-list',
-	},
-	{
-		routePath: '#/detail',
-		view: '#content-detail'
-	}
-];
+	var startupUrl = '#/list';
+
+	var allRoutes =
+	[
+		{
+			routePath: '#/list',
+			view: '#content-list',
+		},
+		{
+			routePath: '#/detail',
+			view: '#content-detail'
+		}
+	];
+
+	return {
+		startupUrl: startupUrl,
+		routes: allRoutes,
+	};
+
+}
