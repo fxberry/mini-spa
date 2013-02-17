0c333a413e30f82af7ae653b9c8fbcc6214efacc
felix egli <antgeneral@gmail.com>
2013-02-17 17:17:38 +0100

    cleanup spaces imports ..

--- Mini-SPA/App_Start/RouteConfig.cs
+++ Mini-SPA/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Mini_SPA
@@ -18,6 +14,8 @@ namespace Mini_SPA
 				url: "{controller}/{action}/{id}",
 				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 			);
+
+
 		}
 	}
 }
