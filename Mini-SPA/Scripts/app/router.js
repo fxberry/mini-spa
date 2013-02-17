d5e610389eab60733eabc1089eeeba5b81fae9f2
felix egli <antgeneral@gmail.com>
2013-02-17 14:21:18 +0100

    put router in a separate module

--- Mini-SPA/Scripts/app/router.js
+++ Mini-SPA/Scripts/app/router.js
@@ -1,30 +1,77 @@
-﻿$(document).ready(function() {
+﻿var router = function () {
+
+	var sammy;
+
+	var init = function () {
+		sammy = new Sammy.Application(function () { });
+	};
 
 	function hideAllContent() {
 		$.each(config.routes, function (index, value) {
 			$(value.view).hide();
 		});
 	}
-	
-	var sammy = new Sammy.Application(function() {});
-	
-	$.each(config.routes, function (index, value) {
-		sammy.get(value.routePath, function (context) {
-			hideAllContent();
-			$(value.view).show();
 
-			context.log('Routed to: ' + value.routePath);
+	var registerRoutes = function (routes) {
+		$.each(routes, function (index, value) {
+			sammy.get(value.routePath, function (context) {
+				hideAllContent();
+				$(value.view).show();
 
-			//toastr.info('Routed to: ' + value.routePath, 'Route');
+				context.log('Routed to: ' + value.routePath);
+
+				//toastr.info('Routed to: ' + value.routePath, 'Route');
+			});
 		});
-	});
 
-	// startuproute
-	sammy.get('', function () {
-		this.app.runRoute('get', config.startupUrl);
-	});
+	};
 
-	sammy.run();
+	var setStartupRouteAndRun = function (startupRoute) {
+		// startuproute
+		sammy.get('', function () {
+			this.app.runRoute('get', startupRoute);
+		});
+
+		sammy.run();
+	};
+
+	init();
+
+	return {
+		registerRouter: registerRoutes,
+		setStartupRouteAndRun: setStartupRouteAndRun
+	};
+
+}();
+
+
+$(document).ready(function () {
+
+	//function hideAllContent() {
+	//	$.each(config.routes, function (index, value) {
+	//		$(value.view).hide();
+	//	});
+	//}
+	
+	
+	
+	//$.each(config.routes, function (index, value) {
+	//	sammy.get(value.routePath, function (context) {
+	//		hideAllContent();
+	//		$(value.view).show();
+
+	//		context.log('Routed to: ' + value.routePath);
+
+	//		//toastr.info('Routed to: ' + value.routePath, 'Route');
+	//	});
+	//});
+
+	//// startuproute
+	//sammy.get('', function () {
+	//	this.app.runRoute('get', config.startupUrl);
+	//});
+
+	//sammy.run();
 
 
 });
