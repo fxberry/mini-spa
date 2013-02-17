d1c752d7e643cf2366e1a0b089e71ba645577551
felix egli <antgeneral@gmail.com>
2013-02-17 12:58:37 +0100

    simplest navigation with sammy log console

---
+++ Mini-SPA/Scripts/app/router.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function() {
+
+	var startupUrl = '#/list';
+
+
+	var sammy = new Sammy.Application(function() {
+
+		this.get('#/list', function (context) {
+			context.log('list');
+		});
+		
+
+		this.get('#/detail', function (context) {
+			context.log('detail');
+		});
+
+		this.get('', function () {
+			this.app.runRoute('get', startupUrl);
+		});
+		
+	});
+	
+	sammy.run();
+
+
+});
