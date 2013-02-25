c2bcf142442de1d7acd839f76988a94f089c821c
felix egli <antgeneral@gmail.com>
2013-02-25 21:31:21 +0100

    Added Bundling and RequireJS

---
+++ Mini-SPA/App_Start/BundleConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Mini_SPA
+{
+	public class BundleConfig
+    {
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.UseCdn = false;
+
+
+			bundles.IgnoreList.Clear();
+			bundles.IgnoreList.Ignore("*-vsdoc.js");
+			bundles.IgnoreList.Ignore("*intellisense.js");
+
+			// jQuery
+			bundles.Add(new ScriptBundle("~/bundles/jquery")
+				.Include("~/Scripts/lib/jquery-{version}.js"));
+
+			// 3rd Party JavaScript files
+			bundles.Add(new ScriptBundle("~/bundles/jsextlibs")
+				            .Include(
+					            "~/Scripts/lib/knockout-{version}.js",
+					            "~/Scripts/lib/sammy-{version}.js"
+				            ));
+
+			// All application Javascript
+			bundles.Add(new ScriptBundle("~/bundles/jsapplibs")
+				.IncludeDirectory("~/Scripts/app/", "*.js", searchSubdirectories: false));
+
+			// CSS
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+				"~/Content/app.css",
+				"~/Content/toastr.css"));
+		}
+    }
+}
--- Mini-SPA/Global.asax.cs
+++ Mini-SPA/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using Newtonsoft.Json.Serialization;
 
@@ -16,6 +17,8 @@ namespace Mini_SPA
 			WebApiConfig.Register(GlobalConfiguration.Configuration);
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
 
 			GlobalConfig.CustomizeConfiguration(GlobalConfiguration.Configuration);
 		}
