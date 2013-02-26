e96c5908d1a1e04758c9daa241f610217217d13b
felix egli <antgeneral@gmail.com>
2013-02-26 22:28:16 +0100

    working on qunit tests

---
+++ Mini-SPA/Tests/testsuite.js
@@ -0,0 +1,40 @@
+﻿(function () {
+
+	// Defer Qunit so RequireJS can work its magic and resolve all modules.
+	QUnit.config.autostart = false;
+
+	var root = this;
+
+	requirejs.config(
+        {
+        	// Let require.js load all app/custom modules asynchronously as needed.
+        	// They are all in this folder.
+        	// If we bundle this folder, this is not needed. But if we don't bundle, we need this.
+        	baseUrl: '../Scripts/app' /* script default location */
+        }
+    );
+
+	function registerNonAmdLibs() {
+		// Load the 3rd party libraries that the app needs.
+		// These are in the bundle (BundleConfig.cs).
+		// These are the core libraries that many others depend on.
+		define('jquery', [], function () { return root.jQuery; });
+		define('ko', [], function () { return root.ko; });
+		define('sammy', [], function () { return root.Sammy; });
+	}
+
+	// Load the 3rd party libraries
+	registerNonAmdLibs();
+
+
+	// A list of all QUnit test Modules.  Make sure you include the `.js` 
+	// extension so RequireJS resolves them as relative paths rather than using
+	// the `baseUrl` value supplied above.
+	var testModules = [
+		"vm.search.tests.js"
+	];
+
+
+	// Resolve all testModules and then start the Test Runner.
+	require(testModules, QUnit.start);
+}());
---
+++ Mini-SPA/Tests/vm.search.tests.js
@@ -0,0 +1,24 @@
+﻿define(function (require) {
+
+	// Import depdendencies.
+	var config = require('config');
+
+	// Define the QUnit module and lifecycle.
+	QUnit.module("My Module", {
+		setup: function () {
+		},
+		teardown: function () {
+		}
+	});
+
+	QUnit.test("check startupUrl is correct", function () {
+		
+		QUnit.equal(config.startupUrl, "#/list");
+		
+	});
+	
+	QUnit.test("isSuitibleFor - same age supplied, returns true", function () {
+		QUnit.equal(true, true);
+	});
+
+});
