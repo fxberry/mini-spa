3a358ab4dd82e80e7a0e8a764ce3bfc4f8b3b56a
felix egli <antgeneral@gmail.com>
2013-02-27 21:43:21 +0100

    finisehd unittesting!! wohoo

--- Mini-SPA/Tests/vm.search.tests.js
+++ Mini-SPA/Tests/vm.search.tests.js
@@ -1,10 +1,24 @@
-﻿define(function (require, qUnit) {
+﻿define('dataservice', [], function () {
+	var messages =
+	[
+		{ id: 1, subject: 'first message', body: 'testbody', isRead: false },
+		{ id: 2, subject: 'second message', body: 'testbody', isRead: false }
+	];
 
+	var getMessages = function(callbacks) {
+		callbacks.success(messages);
+	};
+	return { getMessages: getMessages };
+
+});
+
+
+define(function (require, qUnit) {
 
 	// Import depdendencies.
-	var sut = require('config');
+	var config = require('config');
 	var vmSearch = require('vm.search');
-
+	
 	// Define the QUnit module and lifecycle.
 	QUnit.module("VM Search Tests", {
 		setup: function () {
@@ -13,35 +27,34 @@
 		}
 	});
 
-	QUnit.test("check startupUrl is correct", function () {
-		
-		QUnit.equal(sut.startupUrl, "#/list");
-		
+	QUnit.test("sanity test true=true", function () {
+		equal(true, true);
+	});
+
+	QUnit.test("check correct startupUrl is correct", function () {
+		deepEqual(config.startupUrl, "#/list");
 	});
 	
 	QUnit.test("Viewmodel is not undefined", function () {
-
-		QUnit.ok(vmSearch != undefined);
-
+		ok(vmSearch != undefined);
 	});
 
-	QUnit.asyncTest("contains data", function () {
+	QUnit.asyncTest("Viewmodel messages contains two messages", function () {
+		expect(1);
+		
+		ok(vmSearch.messages().length == 2);
+	});
+	
+	QUnit.test("First subject contains hello", function () {
 		expect(1);
 
-		//vmSearch.init();
-
-		//setTimeout(function () {
-			//ok(true, "Passed and ready to resume!");
-			
-			ok(vmSearch.messages().length > 0);
-			start();
-		//}, 1000);
-
+		equal('first message', vmSearch.messages()[0].subject());
 	});
 
-	
-	QUnit.test("dummy test true=true", function () {
-		QUnit.equal(true, true);
+	QUnit.test("filter startswith test", function () {
+		vmSearch.filter('first');
+
+		equal(1, vmSearch.messages().length);
 	});
 
 });
